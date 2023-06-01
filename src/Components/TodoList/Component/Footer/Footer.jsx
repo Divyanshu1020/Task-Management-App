91e25b16eec934cd317fb248d13ddc5f8fb0662a
Divyanshu <divyanshuvishwakarma222@gmail.com>
2023-06-01 21:25:00 +0530

    Add Delete functionality

--- src/Components/TodoList/Component/Footer/Footer.jsx
+++ src/Components/TodoList/Component/Footer/Footer.jsx
@@ -1,11 +1,26 @@
-import React from 'react'
+import React,{useState,useContext} from 'react'
 import "./Footer.css"
+import { DataContext } from '../../DataProvider/DataProvider'
 
 export default function Footer() {
+  const [check, setCheck] = useState(false)
+  const [todos,setTodos] = useContext(DataContext)
+  const deleteFunction =()=>{
+    const newTodo = todos.filter(todo => {
+      if(!todo.complete){
+        return todo
+      }
+    
+    })
+
+    setTodos(newTodo)
+    
+  }
   return (
     <div className='row'>
       <label htmlFor="all">
         <input
+          checked={check}
           className='checkbox'
           type="checkbox"
           name='all'
@@ -13,7 +28,7 @@ export default function Footer() {
         All
       </label>
       <p>You have 0 to do</p>
-      <button id='delete'>Delete</button>
+      <button id='delete' onClick={deleteFunction}>Delete</button>
     </div>
   )
 }
