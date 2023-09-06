3a859d488f46f149350a1765fe6b32f8d198f4f1
Divyanshu <divyanshuvishwakarma222@gmail.com>
2023-09-06 16:27:40 +0530

    same check

---
+++ Task Management App/src/config/firebase.js
@@ -0,0 +1,24 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAuth } from "firebase/auth";
+import { getDatabase } from "firebase/database";
+import { getFirestore } from "firebase/firestore";
+
+
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setun up#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyDWFTF6IXs6WkIeK82TjQefuVH4ouQYf7s",
+  authDomain: "todo-5e980.firebaseapp.com",
+  projectId: "todo-5e980",
+  storageBucket: "todo-5e980.appspot.com",
+  messagingSenderId: "178251877436",
+  appId: "1:178251877436:web:6fae9b25cf75554d350739"
+};
+
+// Initialize Firebase
+export const app = initializeApp(firebaseConfig);
+export const db = getFirestore(app)
+export const auth = getAuth();
